d0ed4cddda413dc63155302bb180a8789113784b
Jair-F <jair.fehlauer@gmail.com>
2026-07-15 22:22:25 +0300

    -

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# working
-./sync.py logs/00000018.BIN logs/2026-05-23\ 19-06-38.tlog logs/log.mcap # --no-overlap-check
-
-# failing
-./sync.py logs/ArduPlane-GpsSensorPreArmEAHRS-00000140.BIN logs/ArduPlane-test.tlog logs/log.mcap  # --no-overlap-check
